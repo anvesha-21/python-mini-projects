0dce89f1c4e7bfe824eababcf4b8af4556417095
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-07 22:05:33 +0530

    Add files via upload
    A simple command-line calculator that supports basic arithmetic operations. |

---
+++ calculator.py
@@ -0,0 +1,48 @@
+#calculator
+
+
+print("*****************************************CALCULATOR**************************************************")
+
+
+
+
+
+
+#Addition
+print("Addition")
+a=int(input("Enter 1 number:"))
+b=int(input("Enter 2 number:"))
+print(a+b) 
+
+
+#Subtraction
+print("Subtraction")
+a=int(input("Enter 1 number:"))
+b=int(input("Enter 2 number:"))
+print(a-b)
+
+#Multiplication
+print("Multiplication")
+a=int(input("Enter 1 number:"))
+b=int(input("Enter 2 number:"))
+print(a*b)
+
+#Division
+print("Division")
+a=int(input("Enter 1 number:"))
+b=int(input("Enter 2 number:"))
+print(a/b)
+
+#Floor division
+print("Floor Division")
+a=int(input("Enter 1 number:"))
+b=int(input("Enter 2 number:"))
+print(a//b)
+
+#Modulas
+print("Modulas")
+a=int(input("Enter 1 number:"))
+b=int(input("Enter 2 number:"))
+print(a%b)
+
+
