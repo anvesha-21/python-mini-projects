3aca70a00511adb855a4636c884b6306379d50e8
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-07 22:54:55 +0530

    Add files via upload
    A basic multiple-choice quiz

---
+++ Exercise1KBC.py
@@ -0,0 +1,303 @@
+# Create a program capable of diaplaying questions to user like KBC.
+
+# import threading
+# from playsound import playsound
+# import time
+
+# # Function to play background music
+# def play_music():
+#     playsound("kbc-awesome-.py")  # or .wav if you're using wav
+
+# # Start the music in background
+# music_thread = threading.Thread(target=play_music)
+# music_thread.start()
+
+# # Your KBC game code below
+# print("🎤 Welcome to Kaun Banega Crorepati!\n")
+# time.sleep(1)
+# print("Here is your first question...\n")
+
+# # # Simulate game (replace with your game logic)
+# # for i in range(5):
+# #     print(f"Question {i+1}")
+# #     time.sleep(2)
+
+
+
+
+
+
+
+print("**********************************************************************DEVIYO aur SAJJANO SWAGAT HAI AAPKA *******************************************************")
+print("***************************************************************************KON BANEGA CROREPATI MAI**************************************************************")
+string = input("  Aapka Subh Naam :- ")
+print("Hello.." , string)
+print("Welcome to Kaun Banega Crorepati – where every answer brings you closer to your crore-dream!\n\n")
+
+
+
+
+# TrialQ
+
+print("TrialQ.Which one of the following is not an animal ? \n")
+
+a="DOG"
+print("Option1:- " , a)
+b="CAT"
+print("Option2:- " , b)
+c="HUMAN"
+print("Option3:- " , c)
+d="TIGER"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 3):
+    print("RIGHT ANSWER:" , c)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , c )
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+    
+            
+
+
+
+
+
+
+# Question1
+
+print("\nQ1.Who was the first Indian to win an individual Olympic gold medal? \n")
+a="Leander Paes"
+print("Option1:- " , a)
+b="Abhinav Bindra"
+print("Option2:- " , b)
+c="Rajyavardhan Singh Rathore"
+print("Option3:- " , c)
+d="Karnam Malleswari\n"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 2):
+    print("RIGHT ANSWER:" , b)
+else:
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , b)
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+    
+
+
+
+# # Question2
+
+print("\nQ2. In which year did the Battle of Buxar take place? \n")
+a="1757"
+print("Option1:- " , a)
+b="1761"
+print("Option2:- " , b)
+c="1764"
+print("Option3:- " , c)
+d="1775\n"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 3):
+    print("RIGHT ANSWER:" , c)
+else:
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , c )
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+
+# # Question3
+
+print("\nQ3.Who is credited with the discovery of the neutron \n")
+a="James Chadwick"
+print("Option1:- " , a)
+b="Ernest Rutherford"
+print("Option2:- " , b)
+c="JJ.Thomson"
+print("Option3:- " , c)
+d="Niels Bohr\n"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 1):
+    print("RIGHT ANSWER:" , a)
+else:
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , a )
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+ 
+#  Quesion4
+
+print("\nQ4. Which Indian classical musician was awarded Bharat Ratna in 1999 posthumously? \n")
+a="Bismillah Khan"
+print("Option1:- " , a)
+b="M.S Subbulakshmi"
+print("Option2:- " , b)
+c="Pandit Bhimsen Joshi"
+print("Option3:- " , c)
+d="Amjad Ali Khan\n"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 1):
+    print("RIGHT ANSWER:" , a)
+else:
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , a )
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+ 
+
+# Question5
+
+ print("Q5.Which one of the following is not an animal ? \n")
+
+a="Aishwarya Rai"
+print("Option1:- " , a)
+b="Sushmita Sen"
+print("Option2:- " , b)
+c="Reita Faria"
+print("Option3:- " , c)
+d="Diana Hayden"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 3):
+    print("RIGHT ANSWER:" , c)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , c )
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+ 
+#  Question6
+
+print("Q6. Who was the founder of the Maurya Empire?\n")
+
+a="Ashoka"
+print("Option1:- " , a)
+b="Chandragupta Maurya"
+print("Option2:- " , b)
+c="Bindusara"
+print("Option3:- " , c)
+d="Kautilya"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 2):
+    print("RIGHT ANSWER:" , b)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , b)
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+ 
+#  Question7
+
+
+print("Q7. Who was the first Indian woman to become the President of the Indian National Congress? \n")
+a="Annie Besant"
+print("Option1:- " , a)
+b="Sarojini Naidu"
+print("Option2:- " , b)
+c="Sucheta Kriplani"
+print("Option3:- " , c)
+d="Indira Gandhi"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 1):
+    print("RIGHT ANSWER:" , a)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , a)
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+ 
+#  Question8
+
+print("Q8. In which year did the Jallianwala Bagh massacre occur?\n")
+
+a="1971"
+print("Option1:- " , a)
+b="1919"
+print("Option2:- " , b)
+c="1920"
+print("Option3:- " , c)
+d="1921"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 2):
+    print("RIGHT ANSWER:" , b)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , b)
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+ 
+#  Question9
+
+print("Q9. Who was the last Governor-General of independent India?\n")
+
+a="C.Rajagopalachari"
+print("Option1:- " , a)
+b="Lord Mountbatten"
+print("Option2:- " , b)
+c="Jawaharlal Nehru"
+print("Option3:- " , c)
+d="Sardar Patel"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 1):
+    print("RIGHT ANSWER:" , a)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , a)
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+
+# Question10
+
+print("Q10.Which revolutionary was associated with the \"Delhi Conspiracy Case\"?\n")
+
+a="Bhagat Singh"
+print("Option1:- " , a)
+b="Ram Prasad Bismil"
+print("Option2:- " , b)
+c="Rash Behari Bose"
+print("Option3:- " , c)
+d="Chandrashekhar Azad"
+print("Option4:- " , d)
+
+user = int (input("Answer :- "))
+if(user == 3):
+    print("RIGHT ANSWER:" , c)
+else :
+ print("WRONG ANSWER!!!")
+ print("\nCORRECT ANSWER:- " , c)
+ exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
+          ******PLEASE TRY AGAIN LATER*****
+                <3<3THANK YOUUUUUU(:(: """  )
+
+
+
+
+    
