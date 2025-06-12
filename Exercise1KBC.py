52c43527d8b04340359ed2b70c60769cd280cf9c
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-12 18:16:20 +0530

    Update Exercise1KBC.py

--- Exercise1KBC.py
+++ Exercise1KBC.py
@@ -10,267 +10,68 @@ print("Welcome to Kaun Banega Crorepati – where every answer brings you closer
 
 
 
-# TrialQ
+questions =[
 
-print("TrialQ.Which one of the following is not an animal ? \n")
+["Q1. Which language was used to create Facebook?" , "Python" , "French" , "Hindi" , "Php" , "None" , 4],
 
-a="DOG"
-print("Option1:- " , a)
-b="CAT"
-print("Option2:- " , b)
-c="HUMAN"
-print("Option3:- " , c)
-d="TIGER"
-print("Option4:- " , d)
+["Q2. Who was the first Indian to win an individual Olympic gold medal? " , "Leander Paes" ,"Abhinav Bindra" ,  "Rajyavardhan Singh Rathore" , "Karnam Malleswari" , 2 ] , 
 
-user = int (input("Answer :- "))
-if(user == 3):
-    print("RIGHT ANSWER:" , c)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , c )
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
-    
-            
+["Q3. In which year did the Battle of Buxar take place?" , "1757" , "1761" ,  "1764" , "1775" , 3],
 
+["Q4.Who is credited with the discovery of the neutron" , "James Chadwick" ,  "Ernest Rutherford" , "JJ.Thomson" ,  "Niels Bohr" , 1],
 
+["Q5. Which Indian classical musician was awarded Bharat Ratna in 1999 posthumously?" , "Bismillah Khan" ,  "M.S Subbulakshmi" , "Pandit Bhimsen Joshi" , "Amjad Ali Khan"],
 
+["Q6.Who was the first Prime Minister of independent India?" , ". Subhas Chandra Bose" , " Jawaharlal Nehru" , ". Mahatma Gandhi" , "Dr. Rajendra Prasad" , 2 ],
 
+["Q7.What movement did Mahatma Gandhi launch in 1942?", "Non-Cooperation Movement" , "Salt March" , "Quit India Movement" , "Swadeshi Movement" , 3],
 
+["Q8.Which Article of the Indian Constitution grants special status to Jammu and Kashmir (before 2019)?", ". Article 356" , "Article 360" , ". Article 370" , ". Article 42" , 3 ],
 
-# Question1
+["Q9.Who was the first woman Prime Minister of India?" , "Sarojini Naidu" , " Indira Gandhi" , ". Pratibha Patil" , " Sushma Swaraj" , 2],
 
-print("\nQ1.Who was the first Indian to win an individual Olympic gold medal? \n")
-a="Leander Paes"
-print("Option1:- " , a)
-b="Abhinav Bindra"
-print("Option2:- " , b)
-c="Rajyavardhan Singh Rathore"
-print("Option3:- " , c)
-d="Karnam Malleswari\n"
-print("Option4:- " , d)
+["Q10.Which Indian freedom fighter formed the Indian National Army (INA)?", "Bhagat Singh" , ". Subhas Chandra Bose" , "Sardar Vallabhbhai Patel" , " Jawaharlal Nehru" , 2],
 
-user = int (input("Answer :- "))
-if(user == 2):
-    print("RIGHT ANSWER:" , b)
-else:
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , b)
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
-    
+["Q11.Who was the President of India during the Emergency (1975-77)?", " V.V. Giri" , ". Dr. Rajendra Prasad" , "Fakhruddin Ali Ahmed" , "R. Venkataraman" ,3 ],
+
+["Q12.Which Indian Act first gave a limited franchise to Indians in British India?", "Regulating Act, 1773" , "Charter Act, 1833" , "Government of India Act, 1919" , ". Government of India Act, 1935" ,3 ],
+
+["Q13.The Simon Commission was boycotted by Indians because:", " It had too many Indians" , " It was appointed during war" , " It had no Indian member" , " It was headed by a viceroy" ,3 ],
+
+["Q14.The first Indian Governor-General of independent India was:", " Lord Mountbatten" , "Dr. B.R. Ambedkar" , " C. Rajagopalachari" , "S. Radhakrishnan" , 3],
+
+["Q15.Q: Which of the following leaders was not present in the Constituent Assembly of India on 26th November 1949, the day the Constitution was adopted?", " Dr. B.R. Ambedkar" , "Jawaharlal Nehru" , ". Mahatma Gandhi" , "Dr. Rajendra Prasad" , 3],
 
 
+] 
 
-# # Question2
-
-print("\nQ2. In which year did the Battle of Buxar take place? \n")
-a="1757"
-print("Option1:- " , a)
-b="1761"
-print("Option2:- " , b)
-c="1764"
-print("Option3:- " , c)
-d="1775\n"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 3):
-    print("RIGHT ANSWER:" , c)
-else:
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , c )
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
-
-# # Question3
-
-print("\nQ3.Who is credited with the discovery of the neutron \n")
-a="James Chadwick"
-print("Option1:- " , a)
-b="Ernest Rutherford"
-print("Option2:- " , b)
-c="JJ.Thomson"
-print("Option3:- " , c)
-d="Niels Bohr\n"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 1):
-    print("RIGHT ANSWER:" , a)
-else:
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , a )
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
- 
-#  Quesion4
-
-print("\nQ4. Which Indian classical musician was awarded Bharat Ratna in 1999 posthumously? \n")
-a="Bismillah Khan"
-print("Option1:- " , a)
-b="M.S Subbulakshmi"
-print("Option2:- " , b)
-c="Pandit Bhimsen Joshi"
-print("Option3:- " , c)
-d="Amjad Ali Khan\n"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 1):
-    print("RIGHT ANSWER:" , a)
-else:
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , a )
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
- 
-
-# Question5
-
- print("Q5.Which one of the following is not an animal ? \n")
-
-a="Aishwarya Rai"
-print("Option1:- " , a)
-b="Sushmita Sen"
-print("Option2:- " , b)
-c="Reita Faria"
-print("Option3:- " , c)
-d="Diana Hayden"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 3):
-    print("RIGHT ANSWER:" , c)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , c )
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
- 
-#  Question6
-
-print("Q6. Who was the founder of the Maurya Empire?\n")
-
-a="Ashoka"
-print("Option1:- " , a)
-b="Chandragupta Maurya"
-print("Option2:- " , b)
-c="Bindusara"
-print("Option3:- " , c)
-d="Kautilya"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 2):
-    print("RIGHT ANSWER:" , b)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , b)
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
- 
-#  Question7
+levels = [1000 , 2000 , 3000 , 5000 , 10000 , 20000 , 40000 , 80000 , 160000 , 320000 , 640000 , 1250000 , 2500000 , 5000000 , 10000000]
+money = 0
 
 
-print("Q7. Who was the first Indian woman to become the President of the Indian National Congress? \n")
-a="Annie Besant"
-print("Option1:- " , a)
-b="Sarojini Naidu"
-print("Option2:- " , b)
-c="Sucheta Kriplani"
-print("Option3:- " , c)
-d="Indira Gandhi"
-print("Option4:- " , d)
+for i  in range (0 , len (questions)):
+    question = questions[i]
+    print("")
+    print(f"Question for Rs.{levels[i]}")
+    print(question[0])
+    print(f"a.{question[1]}                 b.{question[2]}   ")
+    print(f"c.{question[3]}                 d.{question[4]}   ")
+    reply = int(input("Enter your answer (1-4) or 0 to Quit : \n"))
+    if(reply == 0):
+        money = levels[i-1]
+        break
+    if (reply == question[-1]):
+        print(f"Correct answer , you have won Rs.{levels[i]}\n")
+        if (i==4):
+            money = 10000
+        elif(i==9):
+            money = 320000
+        elif(i==14):
+            money = 10000000
 
-user = int (input("Answer :- "))
-if(user == 1):
-    print("RIGHT ANSWER:" , a)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , a)
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
- 
-#  Question8
-
-print("Q8. In which year did the Jallianwala Bagh massacre occur?\n")
-
-a="1971"
-print("Option1:- " , a)
-b="1919"
-print("Option2:- " , b)
-c="1920"
-print("Option3:- " , c)
-d="1921"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 2):
-    print("RIGHT ANSWER:" , b)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , b)
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
- 
-#  Question9
-
-print("Q9. Who was the last Governor-General of independent India?\n")
-
-a="C.Rajagopalachari"
-print("Option1:- " , a)
-b="Lord Mountbatten"
-print("Option2:- " , b)
-c="Jawaharlal Nehru"
-print("Option3:- " , c)
-d="Sardar Patel"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 1):
-    print("RIGHT ANSWER:" , a)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , a)
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
-
-# Question10
-
-print("Q10.Which revolutionary was associated with the \"Delhi Conspiracy Case\"?\n")
-
-a="Bhagat Singh"
-print("Option1:- " , a)
-b="Ram Prasad Bismil"
-print("Option2:- " , b)
-c="Rash Behari Bose"
-print("Option3:- " , c)
-d="Chandrashekhar Azad"
-print("Option4:- " , d)
-
-user = int (input("Answer :- "))
-if(user == 3):
-    print("RIGHT ANSWER:" , c)
-else :
- print("WRONG ANSWER!!!")
- print("\nCORRECT ANSWER:- " , c)
- exit ("""         *****WE APPRECIATE YOUR EFFORTS*****
-          ******PLEASE TRY AGAIN LATER*****
-                <3<3THANK YOUUUUUU(:(: """  )
+    else:
+        print("Wrong Answer!")
+        break
 
 
-
-
-    
+print(f"AAPNE JITTE HAI {money}")
