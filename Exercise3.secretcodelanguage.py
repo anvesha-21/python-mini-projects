21e82a90edcbc95ea9a21c397266325fa7206b5d
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-06-24 23:26:41 +0530

    Add files via upload

---
+++ Exercise3.secretcodelanguage.py
@@ -0,0 +1,85 @@
+# dic = {'name' }   #'anvesha' , 'age' : 19 }
+# dic2 ={'nasmem' }   #'anvesmeshasma' , 'asmeg' : 'nismightenn nusmumber mai'}
+# # print (dic2.remove("sm"))
+# dic = ("What you are doing write now")
+# dic2 =("hatw oyu rea iondg rtiew own")
+# print(dic2)
+# print("HINT : All the letters are placed in incorrect way correct it and rewrite the sentence and decode it ...")
+# user = input("Decode the message: ")
+
+
+
+
+# #decoding
+# if(user==dic):
+#     print("Correct Decoding")
+# else:
+#     print("Failed")
+
+
+# # coding
+# str=("sheanvsh")
+
+# str2 = ("Anvesha")
+
+# print(str)
+
+# user = input("Decode the code : ")
+
+# list= {"ses" , "on"}
+
+# if(user==str2):
+#       print("User Succesfully decoded ...")
+# user1 = input("If you understand the it give answer in a coded form ")
+# if(user1==list(0) or list(1)):
+#     print("Great")
+# else:
+#  print("failed")
+
+# Code With Harry solution
+
+# st = input("Enter the message:")
+# words = st.split(" ")
+# coding = input("1 for Coding or 0 for decoding: ")
+# coding = True if (coding=="1") else False
+# print(coding)
+# if (coding):
+#     nwords = []
+#     for word in words:
+#         if(len(word)>=3):
+#             r1 = "dsf"
+#             r2 = "jkr"
+#             stnew = r1 + word [1:] + word [0] + r2
+#             nwords.append(stnew)
+#             print(" ".join(nwords))
+#         else:
+#             nwords.append(word[: : -1])
+#             print(" ".join(nwords))
+
+# else:
+#     nwords = []
+#     for word in words:
+#         if(len(word)>=3):
+#             stnew = word[3:-3]
+#             stnew = stnew[-1]+stnew[:-1]
+#             nwords.append(stnew)
+#         else:
+#             nwords.append(word[: : -1])
+#             print(" ".join(nwords))
+         
+
+
+# My created encryption
+
+message = input("Enter a message:")
+
+word = 0
+if word[ 1 : ]:
+    print("bhd")
+
+
+
+
+    
+
+
