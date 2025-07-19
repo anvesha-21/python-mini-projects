27dea4ebb6cdfc9d13e82dd114b5ae43d995c035
Anvesha Chaturvedi <anveshavitthalchaturvedi@gmail.com>
2025-07-19 21:00:08 +0530

    Update Exercise4.secretcodelanguage.py

--- Exercise4.secretcodelanguage.py
+++ Exercise4.secretcodelanguage.py
@@ -1,22 +1,22 @@
-# My Version!
+# # My Version!
 
-dic = {'name' }  
-dic2 ={'nasmem' }  
-# print (dic2.remove("sm"))
-dic = ("What you are doing write now")
-dic2 =("hatW oyu rea iondg rtiew own")
-print(dic2)
-print("HINT : All the letters are placed in incorrect way correct it and rewrite the sentence and decode it ...")
-user = input("Decode the message: ")
-
-
+# dic = {'name' }  
+# dic2 ={'nasmem' }  
+# # print (dic2.remove("sm"))
+# dic = ("What you are doing write now")
+# dic2 =("hatW oyu rea iondg rtiew own")
+# print(dic2)
+# print("HINT : All the letters are placed in incorrect way correct it and rewrite the sentence and decode it ...")
+# user = input("Decode the message: ")
 
 
-#decoding
-if(user==dic):
-    print("Correct Decoding")
-else:
-    print("Failed")
+
+
+# #decoding
+# if(user==dic):
+#     print("Correct Decoding")
+# else:
+#     print("Failed")
 
 
 # coding
@@ -40,34 +40,34 @@ else:
 
 # Modified Version
 
-# st = input("Enter the message:")
-# words = st.split(" ")
-# coding = input("1 for Coding or 0 for decoding: ")
-# coding = True if (coding=="1") else False
-# print(coding)
-# if (coding):
-#     nwords = []
-#     for word in words:
-#         if(len(word)>=3):
-#             r1 = "dsf"
-#             r2 = "jkr"
-#             stnew = r1 + word [1:] + word [0] + r2
-#             nwords.append(stnew)
-#             print(" ".join(nwords))
-#         else:
-#             nwords.append(word[: : -1])
-#             print(" ".join(nwords))
+st = input("Enter the message:")
+words = st.split(" ")
+coding = input("1 for Coding or 0 for decoding: ")
+coding = True if (coding=="1") else False
+print(coding)
+if (coding):
+    nwords = []
+    for word in words:
+        if(len(word)>=3):
+            r1 = "fhv"
+            r2 = "xku"
+            stnew = r1 + word [1:] + word [0] + r2
+            nwords.append(stnew)
+            print(" ".join(nwords))
+        else:
+            nwords.append(word[: : -1])
+            print(" ".join(nwords))
 
-# else:
-#     nwords = []
-#     for word in words:
-#         if(len(word)>=3):
-#             stnew = word[3:-3]
-#             stnew = stnew[-1]+stnew[:-1]
-#             nwords.append(stnew)
-#         else:
-#             nwords.append(word[: : -1])
-#             print(" ".join(nwords))
+else:
+    nwords = []
+    for word in words:
+        if(len(word)>=3):
+            stnew = word[3:-3]
+            stnew = stnew[-1]+stnew[:-1]
+            nwords.append(stnew)
+        else:
+            nwords.append(word[: : -1])
+            print(" ".join(nwords))
          
 
 
